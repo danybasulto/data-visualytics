4c2c0217db9da2b1b0e92e35f16b88466a90e1e3
LandinBT <daniel.basulto0859@alumnos.udg.mx>
2025-10-20 14:14:45 -0600

    feat: implement the parameter selection interface
    Add user controls to the sidebar to configure the analysis:
    - Drop-down menu to select the algorithm (3 options).
    - Multiple selector for attribute variables (x).
    - Simple conditional selector for the target variable (y),
        wich only appears for Linear and Logistic Regression.

--- app.py
+++ app.py
@@ -16,6 +16,26 @@ def main():
             st.success("¡Archivo cargado exitosamente!")
             df_display = df.head(100)
             st.dataframe(df_display)
+            columns = df.columns.tolist()
+            selected_algorithm = st.selectbox(
+                "Seleccione el algoritmo a ejecutar:",
+                ["Regresión Lineal Múltiple", "Regresión Logística Binaria", "K-Means"]
+            )
+            variables_x = st.multiselect(
+                "Seleccione las variables de atributos (x):",
+                options=columns,
+                help="Estas son las variables que el modelo usará para predecir."
+            )
+            variable_y = None
+            if selected_algorithm != "K-Means":
+                # we filter the options for ‘Y’ so that a variable that is already in ‘X’ cannot be selected
+                options_y = [col for col in columns if col not in variables_x]
+
+                variable_y = st.selectbox(
+                    "Seleccione la variable de la calse (y):",
+                    options=options_y,
+                    help="Esta es la variable que el modelo intentará predecir."
+                )
         except Exception as e:
             st.error(f"Error al leer el archivo: {e}")
 
